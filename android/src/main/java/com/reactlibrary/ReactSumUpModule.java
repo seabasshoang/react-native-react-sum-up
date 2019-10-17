fa5b6d6db58b83ac92c99422328b04707eddf6b1
Romano Schneider <rschneider1145@web.de>
2019-10-17 13:54:15 +0200

    Bug fix name

--- android/src/main/java/com/reactlibrary/ReactSumUpModule.java
+++ android/src/main/java/com/reactlibrary/ReactSumUpModule.java
@@ -39,7 +39,7 @@ public class ReactSumUpModule extends ReactContextBaseJavaModule {
 
     @Override
     public String getName() {
-        return "ReactSumUp";
+        return "SumUpBridge";
     }
 
     @ReactMethod
