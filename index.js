d620e57ea14f7acd69ffee6cee5945c236da4f63
Romano Schneider <rschneider1145@web.de>
2019-10-18 09:30:14 +0200

    set optinal Prop

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const SumUpSDK = {
         }
     },
 
-    showLoginViewController(){
+    showLoginViewController(apikey=""){
         if(Platform.OS === 'ios'){
             SumUpBridge.presentLoginFromViewController()
         }else {
