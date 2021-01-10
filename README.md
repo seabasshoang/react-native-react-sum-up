7d254225ddae770cc6d101a95f43c519df5cf361
David Wilson <david.wilson@ami3go.com>
2021-01-10 21:23:30 +0000

    setup the project to use typescript

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 
 ## Getting started
 
-`$ npm install react-native-sumup-interface --save`
+1. `$ npm install react-native-sumup-interface --save`
+2. `$ react-native link react-native-sumup-interface`
 
-### Mostly automatic installation
-
-`$ react-native link react-native-sumup-interface`
-
-### Manual installation
+### Installation
 
 
 #### iOS
@@ -18,26 +15,57 @@
 3. In XCode, in the project navigator, select your project. Add `libReactSumUp.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
 4. Run your project (`Cmd+R`)<
 
+> Please note that iOS is untested. Please feel free to contribute to the project if updates are required
+
 #### Android
 
 1. Open up `android/app/src/main/java/[...]/MainApplication.java`
-  - Add `import com.reactlibrary.ReactSumUpPackage;` to the imports at the top of the file
-  - Add `new ReactSumUpPackage()` to the list returned by the `getPackages()` method
+  - Add `import com.ami3go.sumupinterface.ReactSumUpModule;` to the imports at the top of the file
+  - In the `onCreate()` method, add `ReactSumUpModule.initSumUp(this);` to the end
 2. Append the following lines to `android/settings.gradle`:
   	```
   	include ':react-native-sumup-interface'
   	project(':react-native-sumup-interface').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-sumup-interface/android')
   	```
 3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
-  	```
-      compile project(':react-native-sumup-interface')
+    ```
+    compile project(':react-native-sumup-interface')
   	```
 
 
 ## Usage
-```javascript
+```typescript
 import ReactSumUp from 'react-native-sumup-interface';
 
-// TODO: What to do with the module?
-SumUpSDK;
+SumUpSDK.init('YOUR_API_KEY');
+
+const sumUpSdk = new SumUpSDK();
+```
+
+To authenticate with a token (see https://developer.sumup.com/rest-api/#section/Authentication)...
+```typescript
+await sumUpSdk.authenticateWithToken('YOUR_TOKEN_HERE');
+```
+
+To authenticate with SumUp credentials...
+```typescript
+await sumUpSdk.authenticate();
+```
+
+To wake up the device...
+```typescript
+await sumUpSdk.prepareForCheckout();
+```
+
+To begin the checkout process...
+```typescript
+import { CurrencyCodes } from 'react-native-sumup-interface';
+
+await sumUpSdk.checkout(
+    'ABC', /* the transaction title */
+    1.23, /* the transaction amount */);
+// OPTIONAL FIELDS:
+// currencyCode = CurrencyCodes.GBP (defaulted to GBP)
+// foreignTransactionId = '' (foreign transaction ID, must be unique)
+// token = null (authentication token. Only required if authentication hasn't already been done)
 ```
---
+++ src/index.ts
@@ -0,0 +1,118 @@
+import { NativeModules, Platform } from 'react-native';
+
+const { SumUpBridge } = NativeModules;
+
+export enum CurrencyCodes
+{
+    BGN = SumUpBridge.SMPCurrencyCodeBGN,
+    BRL = SumUpBridge.SMPCurrencyCodeBRL,
+    CHF = SumUpBridge.SMPCurrencyCodeCHF,
+    CLP = (Platform.OS === 'android') ? SumUpBridge.SMPCurrencyCodeCLP : null, // iOS SDK version currently doesn't supports this currency
+    CZK = SumUpBridge.SMPCurrencyCodeCZK,
+    DKK = SumUpBridge.SMPCurrencyCodeDKK,
+    EUR = SumUpBridge.SMPCurrencyCodeEUR,
+    GBP = SumUpBridge.SMPCurrencyCodeGBP,
+    HUF = SumUpBridge.SMPCurrencyCodeHUF,
+    NOK = SumUpBridge.SMPCurrencyCodeNOK,
+    PLN = SumUpBridge.SMPCurrencyCodePLN,
+    RON = SumUpBridge.SMPCurrencyCodeRON,
+    SEK = SumUpBridge.SMPCurrencyCodeSEK,
+    USD = SumUpBridge.SMPCurrencyCodeUSD,
+}
+
+interface LoginResult
+{
+    isLoggedIn: boolean
+}
+
+interface SumUpResult
+{
+    success: boolean
+}
+
+interface CheckoutResult extends SumUpResult
+{
+    resultCode: number,
+    message: string,
+    transactionCode: string,
+    cardType: string,
+    cardLast4Digits: string,
+    installments: number,
+    additionalInfo: any
+}
+
+class SumUpSDK
+{
+    private static _apiKey: string = '';
+
+    public static init(key: string)
+    {
+        SumUpSDK._apiKey = key;
+
+        if (Platform.OS === 'ios')
+            SumUpBridge.setup(key);
+    }
+
+    public authenticate(): Promise<SumUpResult>
+    {
+        return (Platform.OS === 'ios') ? SumUpBridge.authenticate() : SumUpBridge.authenticate(SumUpSDK._apiKey);
+    }
+
+    public authenticateWithToken(token): Promise<SumUpResult>
+    {
+        return (Platform.OS === 'ios') ? SumUpBridge.authenticateWithToken(token) : SumUpBridge.authenticateWithToken(SumUpSDK._apiKey, token);
+    }
+
+    public isLoggedIn(): Promise<LoginResult>
+    {
+        return SumUpBridge.isLoggedIn();
+    }
+
+    public logout(): Promise<boolean>
+    {
+        return SumUpBridge.logout();
+    }
+
+    public prepareForCheckout(): Promise<boolean>
+    {
+        return SumUpBridge.prepareForCheckout();
+    }
+
+    public checkout(
+        title: string,
+        totalAmount: number,
+        currencyCode = CurrencyCodes.GBP,
+        foreignTransactionId = "",
+        token: string = null): Promise<CheckoutResult>
+    {
+        const request = {
+            'title': title,
+            'totalAmount': totalAmount,
+            'currencyCode': currencyCode,
+            "foreignTransactionId": foreignTransactionId
+        };
+
+        return this.isLoggedIn().then(async (result) =>
+        {
+            if (!result.isLoggedIn)
+            {
+                if (!token)
+                    throw new Error('Not logged in. Call "authenticate" or pass the token to this method');
+
+                const authResult = await this.authenticateWithToken(token);
+
+                if (!authResult.success)
+                    throw new Error(`Error while authenticating:${JSON.stringify(authResult)}`);
+            }
+
+            return SumUpBridge.checkout(request);
+        });
+    };
+
+    public preferences(): Promise<SumUpResult>
+    {
+        return SumUpBridge.preferences();
+    }
+};
+
+export default SumUpSDK;
